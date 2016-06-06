e00a4772d28609c3677b1cf513b5ad81f387b383
LIYAO-SZ <liyao-sz@fangdd.com>
2016-06-06 15:01:35 +0800

    update

---
+++ src/ProtoType/SerializableObject.java
@@ -0,0 +1,11 @@
+package ProtoType;
+
+import java.io.Serializable;
+
+/**
+ * Created by LIYAO-SZ on 2016/6/6.
+ */
+public class SerializableObject implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+}
--- src/Singleton/Singleton2.java
+++ src/Singleton/Singleton2.java
@@ -15,7 +15,7 @@ public class Singleton2 {
     //3. 静态工程方法，创建实例：提供给外部用于获取实例的方法
     public static Singleton2 getInstance(){
         if(instance == null){
-            instance = new Singleton2();    //随着用户获取而加载
+            instance = new Singleton2();    //随着用户获取而加载（单线程可用，多线程会出问题）
         }
         return instance;
     }
