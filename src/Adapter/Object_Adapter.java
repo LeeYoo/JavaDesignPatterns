75fcfeae1eb037fde1f57a372a55230a3d49aa05
LIYAO-SZ <liyao-sz@fangdd.com>
2016-06-07 12:03:28 +0800

    updat

--- src/Adapter/Object_Adapter.java
+++ src/Adapter/Object_Adapter.java
@@ -3,11 +3,13 @@ package Adapter;
 /**
  * Created by LIYAO-SZ on 2016/6/6.
  * 【对象的适配器】
+ * 适配器类，直接关联被适配的类。同时实现标准接口
  */
 public class Object_Adapter implements Targetable {
 
-    private Source source;
+    private Source source;//直接关联被适配类
 
+    //通过构造函数传入具体需要适配的被适配类对象
     public Object_Adapter(Source source){
         super();
         this.source = source;
@@ -19,7 +21,7 @@ public class Object_Adapter implements Targetable {
 
     @Override
     public void method1() {
-        source.method1();
+        source.method1();   //使用委托的方式访问
     }
 
 
