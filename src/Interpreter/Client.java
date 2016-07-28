f70fe3cc0eaf7ad8495e5f692ddd4a739396b496
LIYAO-SZ <liyao-sz@fangdd.com>
2016-07-28 18:07:22 +0800

    解释器模式

---
+++ src/Interpreter/Client.java
@@ -0,0 +1,13 @@
+package Interpreter;
+
+/**
+ * Created by LIYAO-SZ on 2016/7/28.
+ * 23 【解释器模式】
+ *      一般主要应用在OOP开发中的编译器的开发中，所以适用面比较窄。
+ *      给定一个语言之后，解释器模式可以定义出其文法的一种表示，并同时提供一个解释器。客户端可以使用这个解释器来解释这个语言中的句子。
+ *
+ * 更多详情介绍：http://www.cnblogs.com/java-my-life/archive/2012/06/19/2552617.html
+ */
+public class Client {
+
+}
